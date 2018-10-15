db03b741f5ec7de94bfd3771290688fc69c4a4c2
darongliu <b01901079@ntu.edu.tw>
2018-10-15 16:55:31 +0800

    finish cpc core module

--- module.py
+++ module.py
@@ -15,36 +15,70 @@ class concat_nn(nn.Module):
         feat_len = feat.size()[1]
         feat_dim = feat.size()[2]
         if feat_len%2 == 1:
-            feat = torch.cat([feat, torch.zeros(feat_batch, 1, feat_dim)], dim=1)
+            pad = torch.zeros([feat_batch, 1, feat_dim])
+            if feat.is_cuda:
+                pad = pad.cuda()
+            feat = torch.cat([feat, pad], dim=1)
             feat_len += 1
         feat = feat.view([feat_batch, int(feat_len/2), feat_dim*2])
 
-        return F.relu(self.linear(feat))
+        return self.linear(F.relu(feat)) #relu -> linear
 
-class count_positive_prob(nn.Module):
-    def __init__(self, c_dim, z_dim):
-        super(count_positive_prob, self).__init__(correct_shift)
-        self.correct_shift = correct_shift
-        self.bilinear_matrix = torch.rand([c_dim, z_dim], require_grad=True)
+class count_NCE_loss(nn.Module):
+    def __init__(self, c_dim, z_dim, prediction_num=12):
+        super(count_NCE_loss, self).__init__()
+        self.prediction_num = prediction_num
+        self.bilinear_matrix = [nn.Parameter(torch.rand([c_dim, z_dim], requires_grad=True)) for _ in range(self.prediction_num)]
+        self.params = nn.ParameterList(self.bilinear_matrix)
+        #self.layers = nn.ModuleList(self.bilinear_matrix)
 
-    def forward(c, shift):
-        Wz_before_shift = torch.matmul(c, self.bilinear_matrix)
+    def forward(self, c, z, neg_shift):
+        '''
+        neg_shift: 1d array with number [self.prediction_num, len]
+        z: [batch x len x z_dim]
+        c: [batch x len x c_dim]
+        '''
+        neg_num = neg_shift.size()[0]
+        negative_z = [self.shift(z, neg_shift[i]) for i in range(neg_num)]
+        all_z = torch.cat([z, *negative_z], dim=0) #[batch*(self.negative+1) x len x z_dim]
 
-def shift(feat, shift):
-    # do not shift vertical
-    # shift hotizontal
-    before = feat[:,:shift,:]
-    after = feat[:,shift:,:]
-    return torch.cat([after, before], dim=1)
+        total_loss = 0
+        for i in range(self.prediction_num):
+            Wc = torch.matmul(c, self.bilinear_matrix[i]) #[batch x len x z_dim]
+            Wc = Wc.repeat(neg_num+1, 1, 1)
+            zWc = torch.sum((Wc*all_z), -1) #[batch*(self.negative+1) x len]
+            zWc = zWc.view([-1, neg_num+1, zWc.size()[-1]])
+            print('zWc: ', zWc.size())
+            f = torch.exp(zWc) # [batch x (self.negative+1) x len]
+            loss  = -torch.log(f[:,0,:]/torch.sum(f[:,1:,:],1)) #[batch x len]
+            total_loss += torch.mean(loss)
+
+        return total_loss
+
+    def shift(self, feat, shift):
+        # do not shift vertical
+        # shift hotizontal
+        before = feat[:,:shift,:]
+        after = feat[:,shift:,:]
+        return torch.cat([after, before], dim=1)
 
 if __name__ == '__main__':
     batch = 2
-    len_ = 2
-    dim = 4
+    len_ = 5
+    dim_c = 4
+    dim_z = 6
 
+    test_feat_c = torch.rand([batch, len_, dim_c], dtype=torch.float32).cuda()
+    test_feat_z = torch.rand([batch, len_, dim_z], dtype=torch.float32).cuda()
+    shift = torch.tensor([1,2,3]).cuda()
+    m = count_NCE_loss(dim_c, dim_z, prediction_num=3)
+    m.cuda()
+    print(m(test_feat_c, test_feat_z, shift))
+    """
     test_feat = torch.rand([batch, len_, dim], dtype=torch.float32).cuda()
     print(test_feat)
     space = torch.tensor(1).cuda()
     m = shift(test_feat, space)
     m = m.cpu()
     print(m)
+    """
